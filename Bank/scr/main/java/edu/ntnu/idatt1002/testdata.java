18670519d56ec0f82da7e25bc0b711a8bd92340a
Elias Tvenning Trana <eliastt@stud.ntnu.no>
2023-02-08 15:25:48 +0100

    Upload New File

---
+++ Bank/scr/main/java/edu/ntnu/idatt1002/testdata.java
@@ -0,0 +1,12 @@
+package piechart.src.main.java.edu.ntnu.idatt1002;
+import java.util.HashMap;
+
+public class testdata {
+    public HashMap<String, Double> testHMap;
+    public testdata(){
+        testHMap = new HashMap<String, Double>();
+        testHMap.put("Hello", 10.5);
+        testHMap.put("Hewdawdlo", 12.5);
+        testHMap.put("Hwadawdlo", 103.5);
+    }
+}
