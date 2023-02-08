bc6a2a33d29d6ba855c48c4c2c81631c9a2882be
Elias Tvenning Trana <eliastt@stud.ntnu.no>
2023-02-08 15:25:34 +0100

    Upload New File

---
+++ Bank/scr/main/java/edu/ntnu/idatt1002/prices.java
@@ -0,0 +1,52 @@
+package piechart.src.main.java.edu.ntnu.idatt1002;
+
+public class prices
+
+{
+    private String name;
+    private double price;
+
+    private int category;
+
+
+    public prices(String name, double price) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void getCategory(int category) {
+        this.category = category;
+    }
+    public void setCategory(int category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "prices{" +
+                "name='" + name + '\'' +
+                ", price=" + price
+                ;
+    }
+}
+
