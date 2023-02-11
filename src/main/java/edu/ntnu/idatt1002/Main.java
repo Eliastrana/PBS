d948f12044215b0d6da72fbb3e579b6bfed18de3
eliastt <eliastt@stud.ntnu.no>
2023-02-11 21:42:51 +0100

    fixed a shell for the create expense part of the program

--- src/main/java/edu/ntnu/idatt1002/Main.java
+++ src/main/java/edu/ntnu/idatt1002/Main.java
@@ -6,8 +6,7 @@ import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.*;
@@ -21,6 +20,8 @@ import static edu.ntnu.idatt1002.testdata.getItem;
 public class Main extends Application {
 
     @Override public void start(Stage stage) {
+
+        //Establish frame
         stage.setTitle("Bank");
         stage.setWidth(1000);
         stage.setHeight(700);
@@ -34,41 +35,8 @@ public class Main extends Application {
         scrollPane.setFitToWidth(true);
         scrollPane.setFitToHeight(false);
 
-        Text text = new Text("Welcome Keira");
-        text.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 80));
-        borderPane.setAlignment(text, Pos.BASELINE_LEFT);
-
-        Text text2 = new Text(timeofdaychecker.timeofdaychecker()+"\n");
-        text2.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 60));
-        text2.setLineSpacing(0);
-        text2.setFill(Color.LIGHTGREEN);
-        borderPane.setAlignment(text2, Pos.BASELINE_LEFT);
-
-        HBox hbox2 = new HBox(2);
-
-        Text textSavings = new Text("Total savings");
-        textSavings.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 40));
-        hbox2.getChildren().add(textSavings);
-
-        Text textSpending = new Text("Monthly spending");
-        textSpending.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 40));
-        hbox2.getChildren().add(textSpending);
-
-        Text text3 = new Text("Scrolling funker");
-        text3.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 80));
-        hbox2.getChildren().add(text3);
-
-        VBox vboxSavings = new VBox(textSavings, new DoughnutChart(pieChartData));
-        vboxSavings.setAlignment(Pos.CENTER);
-
-        VBox vboxSpending = new VBox(textSpending, new DoughnutChart(pieChartData2));
-        vboxSpending.setAlignment(Pos.CENTER);
-
-        HBox hboxPieLayout = new HBox(vboxSavings, vboxSpending);
-        hboxPieLayout.setAlignment(Pos.CENTER);
-
+        //First hbox with menu buttons
         HBox hboxMeny = new HBox(2);
-
 
         Button overview = new Button("Overview");
         overview.setStyle("-fx-background-color: #62de7c; ");
@@ -113,8 +81,102 @@ public class Main extends Application {
         hboxMeny.getChildren().add(more);
         hboxMeny.setAlignment(Pos.CENTER);
 
+        //Welcome text
 
-        VBox vbox = new VBox(hboxMeny, text,text2, hboxPieLayout, text3);
+        Text text = new Text("Welcome Keira");
+        text.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 80));
+        borderPane.setAlignment(text, Pos.BASELINE_LEFT);
+
+        //Time of day text
+        Text text2 = new Text(timeofdaychecker.timeofdaychecker()+"\n");
+        text2.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 60));
+        text2.setLineSpacing(0);
+        text2.setFill(Color.LIGHTGREEN);
+        borderPane.setAlignment(text2, Pos.BASELINE_LEFT);
+
+
+
+        HBox hbox2 = new HBox(2);
+
+        Text textSavings = new Text("Total savings");
+        textSavings.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 40));
+        hbox2.getChildren().add(textSavings);
+
+        Text textSpending = new Text("Monthly spending");
+        textSpending.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 40));
+        hbox2.getChildren().add(textSpending);
+
+        Text emptySpace = new Text("\n");
+        emptySpace.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
+
+        VBox vboxSavings = new VBox(textSavings, new DoughnutChart(pieChartData));
+        vboxSavings.setAlignment(Pos.CENTER);
+
+        VBox vboxSpending = new VBox(textSpending, new DoughnutChart(pieChartData2));
+        vboxSpending.setAlignment(Pos.CENTER);
+
+        HBox hboxPieLayout = new HBox(vboxSavings, vboxSpending);
+        hboxPieLayout.setAlignment(Pos.CENTER);
+
+        HBox newExpenseTitle = new HBox(2);
+        Text text3 = new Text("Add new expense");
+        text3.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 45));
+        newExpenseTitle.setAlignment(Pos.CENTER);
+
+        HBox hboxAddExpenseCategory = new HBox(2);
+
+        Text pickCategory = new Text("Pick a category: ");
+        pickCategory.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
+        hboxAddExpenseCategory.getChildren().add(pickCategory);
+
+        ObservableList<String> options =
+                FXCollections.observableArrayList(
+                        "Food",
+                        "Transportation",
+                        "Clothing",
+                        "Entertainment",
+                        "Other"
+                );
+
+        final ComboBox categoryMenu = new ComboBox(options);
+        hboxAddExpenseCategory.getChildren().add(categoryMenu);
+        hboxAddExpenseCategory.setAlignment(Pos.CENTER);
+
+
+        HBox hboxAddExpensePrice = new HBox(2);
+        hboxAddExpensePrice.setAlignment(Pos.CENTER);
+
+
+        Text pickPrice = new Text("Pick a price: ");
+        pickPrice.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
+        hboxAddExpensePrice.getChildren().add(pickPrice);
+
+        TextField prices = new TextField();
+        prices.setPromptText("Enter price");
+        hboxAddExpensePrice.getChildren().add(prices);
+
+        HBox hboxAddExpenseName = new HBox(2);
+        hboxAddExpenseName.setAlignment(Pos.CENTER);
+
+        Text pickName = new Text("Pick a name: ");
+        pickName.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
+        hboxAddExpenseName.getChildren().add(pickName);
+
+        TextField names = new TextField();
+        names.setPromptText("Enter name");
+        hboxAddExpenseName.getChildren().add(names);
+
+        Button confirmExpense = new Button("Confirm");
+        confirmExpense.setStyle("-fx-background-color: #62de7c; ");
+        confirmExpense.setStyle("-fx-border-width\n: 200; ");
+        confirmExpense.setStyle("-fx-border-heigth\n: 50; ");
+        confirmExpense.setStyle("-fx-font-size\n: 40; ");
+        confirmExpense.setOnAction(e -> {
+            System.out.println("Purchase confirmed");
+        });
+
+
+        VBox vbox = new VBox(hboxMeny, text,text2, hboxPieLayout,emptySpace, newExpenseTitle, hboxAddExpenseCategory, hboxAddExpensePrice, hboxAddExpenseName, confirmExpense);
 
         scrollPane.setContent(vbox);
         borderPane.setCenter(scrollPane);
