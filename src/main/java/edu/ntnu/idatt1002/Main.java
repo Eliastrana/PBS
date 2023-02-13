381fee4fb57f6d10901d4bcc6f38f37d1afa1a7b
eliastt <eliastt@stud.ntnu.no>
2023-02-13 13:04:34 +0100

    Fixed navigation and fixed main menu

--- src/main/java/edu/ntnu/idatt1002/Main.java
+++ src/main/java/edu/ntnu/idatt1002/Main.java
@@ -40,22 +40,70 @@ public class Main extends Application {
         scrollPane.setFitToHeight(false);
         Scene scene = new Scene(borderPane, 1000, 2000);
 
+        //MENUBAR
         MenuBar menuBar = new MenuBar();
         borderPane.setTop(menuBar);
         HBox topMenu = new HBox();
         topMenu.setAlignment(Pos.TOP_LEFT);
 
 
+
+        //FIRST BUTTON
         Button overviewButton = new Button("Overview");
         overviewButton.setOnAction(event -> {
             System.out.println("open overview window");
+            Text text = new Text("Welcome Keira");
+            text.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 80));
+            borderPane.setAlignment(text, Pos.BASELINE_LEFT);
+
+            //Time of day text
+            Text text2 = new Text(timeofdaychecker.timeofdaychecker()+"\n");
+            text2.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 60));
+            text2.setLineSpacing(0);
+            text2.setFill(Color.LIGHTGREEN);
+            borderPane.setAlignment(text2, Pos.BASELINE_LEFT);
+
+
+
+            HBox hbox2 = new HBox(2);
+
+            Text textSavings = new Text("Total savings");
+            textSavings.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 40));
+            hbox2.getChildren().add(textSavings);
+
+            Text textSpending = new Text("Monthly spending");
+            textSpending.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 40));
+            hbox2.getChildren().add(textSpending);
+
+            Text emptySpace = new Text("\n");
+            emptySpace.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
+
+            VBox vboxSavings = new VBox(textSavings, new DoughnutChart(pieChartData));
+            vboxSavings.setAlignment(Pos.CENTER);
+
+            VBox vboxSpending = new VBox(textSpending, new DoughnutChart(pieChartData2));
+            vboxSpending.setAlignment(Pos.CENTER);
+
+            HBox hboxPieLayout = new HBox(vboxSavings, vboxSpending);
+            hboxPieLayout.setAlignment(Pos.CENTER);
+
+
+
+
+            VBox vbox = new VBox(text,text2, hboxPieLayout,emptySpace);
+
+            scrollPane.setContent(vbox);
+            borderPane.setCenter(scrollPane);
+
+            stage.setScene(scene);
+            stage.show();
         });
         topMenu.getChildren().add(overviewButton);
         borderPane.setTop(topMenu);
         overviewButton.setStyle("-fx-font-size: 20px; -fx-min-width: 100px; -fx-min-height: 50px;");
 
 
-
+        //SECOND BUTTON
         Button transferButton = new Button("Transfer");
         transferButton.setOnAction(event -> {
             System.out.println("open transfer window");
@@ -98,14 +146,13 @@ public class Main extends Application {
             rightTransfer.setStyle("-fx-font-size: 20px; -fx-min-width: 100px; -fx-min-height: 50px;");
 
 
-
         });
         topMenu.getChildren().add(transferButton);
         borderPane.setTop(topMenu);
         transferButton.setStyle("-fx-font-size: 20px; -fx-min-width: 100px; -fx-min-height: 50px;");
 
 
-
+        //THIRD BUTTON
         Button payButton = new Button("Pay");
         payButton.setOnAction(event -> {
             //Pay.payWindow();
@@ -122,6 +169,91 @@ public class Main extends Application {
 
 
 
+        //FOURTH BUTTON
+        Button addExpense = new Button("Add expense");
+        addExpense.setOnAction(event -> {
+            System.out.println("open expense window");
+            VBox addExpenseVBox = new VBox();
+            addExpenseVBox.getChildren().add(new Label("This is the addExpense page"));
+            Scene transferScene = new Scene(addExpenseVBox, 800, 600);
+            borderPane.setCenter(transferScene.getRoot());
+
+            HBox newExpenseTitle = new HBox(2);
+            Text text3 = new Text("Add new expense");
+            text3.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 45));
+            newExpenseTitle.setAlignment(Pos.CENTER);
+            newExpenseTitle.getChildren().add(text3);
+            addExpenseVBox.getChildren().add(newExpenseTitle);
+
+
+
+            HBox hboxAddExpenseCategory = new HBox(2);
+
+            Text pickCategory = new Text("Pick a category: ");
+            pickCategory.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
+            hboxAddExpenseCategory.getChildren().add(pickCategory);
+
+            ObservableList<String> options =
+                    FXCollections.observableArrayList(
+                            "Food",
+                            "Transportation",
+                            "Clothing",
+                            "Entertainment",
+                            "Other"
+                    );
+
+            final ComboBox categoryMenu = new ComboBox(options);
+            hboxAddExpenseCategory.getChildren().add(categoryMenu);
+            hboxAddExpenseCategory.setAlignment(Pos.CENTER);
+            addExpenseVBox.getChildren().add(hboxAddExpenseCategory);
+
+
+            HBox hboxAddExpensePrice = new HBox(2);
+            hboxAddExpensePrice.setAlignment(Pos.CENTER);
+
+
+            Text pickPrice = new Text("Pick a price: ");
+            pickPrice.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
+            hboxAddExpensePrice.getChildren().add(pickPrice);
+
+            TextField prices = new TextField();
+            prices.setPromptText("Enter price");
+            hboxAddExpensePrice.getChildren().add(prices);
+
+            addExpenseVBox.getChildren().add(hboxAddExpensePrice);
+
+            HBox hboxAddExpenseName = new HBox(2);
+            hboxAddExpenseName.setAlignment(Pos.CENTER);
+
+            Text pickName = new Text("Pick a name: ");
+            pickName.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
+            hboxAddExpenseName.getChildren().add(pickName);
+
+            TextField names = new TextField();
+            names.setPromptText("Enter name");
+            hboxAddExpenseName.getChildren().add(names);
+
+            addExpenseVBox.getChildren().add(hboxAddExpenseName);
+
+            HBox hboxConfirmExpense = new HBox(2);
+            Button confirmExpense = new Button("Confirm");
+            confirmExpense.setOnAction(e -> {
+                System.out.println("Purchase confirmed");
+            });
+            confirmExpense.setStyle("-fx-font-size: 20px; -fx-min-width: 100px; -fx-min-height: 50px;");
+            hboxConfirmExpense.getChildren().add(confirmExpense);
+            hboxConfirmExpense.setAlignment(Pos.CENTER);
+            addExpenseVBox.getChildren().add(hboxConfirmExpense);
+
+
+        });
+        topMenu.getChildren().add(addExpense);
+        borderPane.setTop(topMenu);
+        addExpense.setStyle("-fx-font-size: 20px; -fx-min-width: 100px; -fx-min-height: 50px;");
+
+
+
+        //FIFTH BUTTON
         Button moreButton = new Button("More");
         moreButton.setOnAction(event -> {
             System.out.println("opening more window");
@@ -137,7 +269,7 @@ public class Main extends Application {
 
 
 
-
+        //HERE STARTS THE OPENING WINDOW
         //Welcome text
 
         Text text = new Text("Welcome Keira");
@@ -175,69 +307,7 @@ public class Main extends Application {
         HBox hboxPieLayout = new HBox(vboxSavings, vboxSpending);
         hboxPieLayout.setAlignment(Pos.CENTER);
 
-        HBox newExpenseTitle = new HBox(2);
-        Text text3 = new Text("Add new expense");
-        text3.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 45));
-        newExpenseTitle.setAlignment(Pos.CENTER);
-        newExpenseTitle.getChildren().add(text3);
-
-
-
-        HBox hboxAddExpenseCategory = new HBox(2);
-
-        Text pickCategory = new Text("Pick a category: ");
-        pickCategory.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
-        hboxAddExpenseCategory.getChildren().add(pickCategory);
-
-        ObservableList<String> options =
-                FXCollections.observableArrayList(
-                        "Food",
-                        "Transportation",
-                        "Clothing",
-                        "Entertainment",
-                        "Other"
-                );
-
-        final ComboBox categoryMenu = new ComboBox(options);
-        hboxAddExpenseCategory.getChildren().add(categoryMenu);
-        hboxAddExpenseCategory.setAlignment(Pos.CENTER);
-
-
-        HBox hboxAddExpensePrice = new HBox(2);
-        hboxAddExpensePrice.setAlignment(Pos.CENTER);
-
-
-        Text pickPrice = new Text("Pick a price: ");
-        pickPrice.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
-        hboxAddExpensePrice.getChildren().add(pickPrice);
-
-        TextField prices = new TextField();
-        prices.setPromptText("Enter price");
-        hboxAddExpensePrice.getChildren().add(prices);
-
-        HBox hboxAddExpenseName = new HBox(2);
-        hboxAddExpenseName.setAlignment(Pos.CENTER);
-
-        Text pickName = new Text("Pick a name: ");
-        pickName.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
-        hboxAddExpenseName.getChildren().add(pickName);
-
-        TextField names = new TextField();
-        names.setPromptText("Enter name");
-        hboxAddExpenseName.getChildren().add(names);
-
-
-        HBox hboxConfirmExpense = new HBox(2);
-        Button confirmExpense = new Button("Confirm");
-        confirmExpense.setOnAction(e -> {
-            System.out.println("Purchase confirmed");
-        });
-        confirmExpense.setStyle("-fx-font-size: 20px; -fx-min-width: 100px; -fx-min-height: 50px;");
-        hboxConfirmExpense.getChildren().add(confirmExpense);
-        hboxConfirmExpense.setAlignment(Pos.CENTER);
-
-
-        VBox vbox = new VBox(text,text2, hboxPieLayout,emptySpace, newExpenseTitle, hboxAddExpenseCategory, hboxAddExpensePrice, hboxAddExpenseName, confirmExpense);
+        VBox vbox = new VBox(text,text2, hboxPieLayout,emptySpace );
 
         scrollPane.setContent(vbox);
         borderPane.setCenter(scrollPane);
@@ -245,11 +315,9 @@ public class Main extends Application {
         stage.setScene(scene);
         stage.show();
 
-
-
-
     }
 
+    //THE FIRST PIE
     private ObservableList<PieChart.Data> createData() {
         return FXCollections.observableArrayList(
                 new PieChart.Data("Card", 2000),
@@ -258,6 +326,7 @@ public class Main extends Application {
 
     }
 
+    //THE SECOND PIE
     private ObservableList<PieChart.Data> createData2() {
         return FXCollections.observableArrayList(
                 new PieChart.Data("Rent" + 1000 + " kr", 13),
@@ -268,6 +337,7 @@ public class Main extends Application {
 
 
     }
+    //MAIN STRING
     //testdata.itemList();
     public static void main(String[] args) {
         HashMap<String, Double> items = new HashMap<>();
