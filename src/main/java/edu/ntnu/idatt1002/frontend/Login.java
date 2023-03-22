f6c995a54187c75469874d3cb0ade39d9ed38497
VegardJohnsen <vegardjohnsen8@gmail.com>
2023-03-22 11:39:40 +0100

    Added one more background

--- src/main/java/edu/ntnu/idatt1002/frontend/Login.java
+++ src/main/java/edu/ntnu/idatt1002/frontend/Login.java
@@ -47,7 +47,7 @@ public class Login {
     background.setPrefSize(1000,700);
 
     Random random = new Random();
-    int randomInt = random.nextInt(1)+1;
+    int randomInt = random.nextInt(2)+1;
     background.getStylesheets().add("/LightMode.css");
     background.getStyleClass().add("loginScreen"+randomInt);
 
