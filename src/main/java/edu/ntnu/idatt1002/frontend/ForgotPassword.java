f9fd57d0c2c42c5ccc397a19151aad5374b04e19
sande <sander@skofsrud.net>
2023-04-12 08:57:50 +0200

    Created ForgotPasswordBackend file and moved all backend from

--- src/main/java/edu/ntnu/idatt1002/frontend/ForgotPassword.java
+++ src/main/java/edu/ntnu/idatt1002/frontend/ForgotPassword.java
@@ -1,10 +1,10 @@
 package edu.ntnu.idatt1002.frontend;
 
-import edu.ntnu.idatt1002.backend.CreateUserBackend;
 import edu.ntnu.idatt1002.backend.LoginObserver;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -15,24 +15,21 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import javax.mail.MessagingException;
-import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.regex.Pattern;
 
+import static edu.ntnu.idatt1002.backend.ForgotPasswordBackend.handleSubmit;
+
 public class ForgotPassword {
 
-    public static boolean changedPassword = false;
     private static List<LoginObserver> observers = new ArrayList<>();
 
-
-    private Stage stage;
     private static TextField emailTextField;
-    private static Label errorLabel;
-    private static String emailString;
-    private static String newPasswordString;
-    private static String masterPasswordString;
+    public static Label errorLabel;
+    public static String emailString;
+    public static String newPasswordString;
     public static boolean backToLogin = false;
     private static final String PASSWORD_PATTERN =
             "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$";
@@ -40,7 +37,6 @@ public class ForgotPassword {
 
 
     public static VBox forgottenPasswordView() {
-
 
         VBox forgottenPasswordVBox = new VBox();
 
@@ -53,18 +49,15 @@ public class ForgotPassword {
         emailTextField.setId("textField");
         emailTextField.setMaxWidth(250);
 
-
         TextField masterPassword = new TextField();
         masterPassword.setPromptText("Enter master password");
         masterPassword.setId("textField");
         masterPassword.setMaxWidth(250);
 
-
         PasswordField newPassword = new PasswordField();
         newPassword.setPromptText("Enter new password");
         newPassword.setId("textField");
         newPassword.setMaxWidth(250);
-
 
         PasswordField confirmNewPassword = new PasswordField();
         confirmNewPassword.setPromptText("Confirm new password");
@@ -101,9 +94,6 @@ public class ForgotPassword {
             }
         });
 
-
-
-
         masterPassword.setVisible(false);
         newPassword.setVisible(false);
         confirmNewPassword.setVisible(false);
@@ -118,19 +108,19 @@ public class ForgotPassword {
         GridPane.setConstraints(submitButton, 1, 2);
         submitButton.setOnAction(e -> {
 
-                Email email = new Email();
-                emailString = emailTextField.getText();
+            Email email = new Email();
+            emailString = emailTextField.getText();
 
-                String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-                StringBuilder sb = new StringBuilder();
-                Random random = new Random();
-                int length = 10;
-                for (int i = 0; i < length; i++) {
-                    int index = random.nextInt(alphabet.length());
-                    char randomChar = alphabet.charAt(index);
-                    sb.append(randomChar);
-                }
-                masterPasswordString = sb.toString();
+            String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            StringBuilder sb = new StringBuilder();
+            Random random = new Random();
+            int length = 10;
+            for (int i = 0; i < length; i++) {
+                int index = random.nextInt(alphabet.length());
+                char randomChar = alphabet.charAt(index);
+                sb.append(randomChar);
+            }
+            String masterPasswordString = sb.toString();
             try {
                 email.sendEmail(emailString, masterPasswordString);
             } catch (MessagingException ex) {
@@ -147,12 +137,11 @@ public class ForgotPassword {
             alert.showAndWait();
         });
 
-
         submitButton.setId("actionButton");
 
         changePasswordButton.setOnAction(e -> {
 
-            if (newPassword.getText().equals(confirmNewPassword.getText()) && masterPassword.getText().equals(masterPasswordString)) {
+            if (newPassword.getText().equals(confirmNewPassword.getText())) {
                 if (!pattern.matcher(newPassword.getText()).matches()) {
                     Alert alert = new Alert(Alert.AlertType.ERROR);
                     alert.setTitle("Error");
@@ -162,8 +151,6 @@ public class ForgotPassword {
                     newPasswordString = newPassword.getText();
                     try {
                         handleSubmit();
-                    } catch (IOException ex) {
-                        throw new RuntimeException(ex);
                     } catch (Exception ex) {
                         throw new RuntimeException(ex);
                     }
@@ -171,12 +158,9 @@ public class ForgotPassword {
             }
         });
 
-
-
         forgottenPasswordVBox.getChildren().addAll(backButtonBox, emailTextField, submitButton, masterPassword, newPassword, confirmNewPassword, changePasswordButton);
         forgottenPasswordVBox.setSpacing(30);
         forgottenPasswordVBox.setAlignment(Pos.CENTER);
-
 
         return forgottenPasswordVBox;
     }
@@ -185,70 +169,11 @@ public class ForgotPassword {
         observers.add(observer);
     }
 
-    private static void notifyObservers() throws Exception {
+    public static void notifyObservers() throws Exception {
         for (LoginObserver observer : observers) {
             observer.update();
             System.out.println("Notified observer");
         }
-    }
-
-    private static void handleSubmit() throws Exception {
-        if (emailString.isEmpty()) {
-            errorLabel.setText("Please enter your email.");
-        } else {
-            String csvFile = "src/main/resources/users.csv";
-            String tempCsvFile = "src/main/resources/temp_users.csv";
-            String line = "";
-            String csvSplitBy = ",";
-            String salt = CreateUserBackend.generateSalt();
-            String newPassword = CreateUserBackend.encrypt(newPasswordString, salt);
-
-            try (BufferedReader br = new BufferedReader(new FileReader(csvFile));
-                 FileWriter fw = new FileWriter(tempCsvFile)) {
-
-                // Loop through each line in the CSV file
-                while ((line = br.readLine()) != null) {
-
-                    // Split the line by commas
-                    String[] user = line.split(csvSplitBy);
-
-                    // Check if the email matches the input email
-                    if (user[3].equals(emailString)) {
-
-                        // Update the password and salt
-                        user[1] = newPassword;
-                        user[2] = salt;
-                    }
-
-                    // Write the line to the temporary CSV file
-                    fw.write(String.join(",", user) + "\n");
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            // Replace the original CSV file with the updated content
-            File originalFile = new File(csvFile);
-            File tempFile = new File(tempCsvFile);
-            if (originalFile.delete()) {
-                if (!tempFile.renameTo(originalFile)) {
-                    throw new IOException("Failed to rename temp file");
-                }
-            } else {
-                throw new IOException("Failed to delete original file");
-            }
-            System.out.println("Password reset");
-            changedPassword = true;
-            notifyObservers();
-        }
-    }
-
-    public static boolean isChangedPassword() {
-        return changedPassword;
-    }
-
-    public static void setChangedPassword(boolean changedPassword) {
-        ForgotPassword.changedPassword = changedPassword;
     }
 
     public static boolean isBackToLogin() {
