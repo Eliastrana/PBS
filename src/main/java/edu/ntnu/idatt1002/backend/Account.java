62fa1ac9c6dfbf812c62c121cf5d7f6c541797a7
sande <sander@skofsrud.net>
2023-04-19 09:09:35 +0200

    Added javadoc

--- src/main/java/edu/ntnu/idatt1002/backend/Account.java
+++ src/main/java/edu/ntnu/idatt1002/backend/Account.java
@@ -2,11 +2,29 @@ package edu.ntnu.idatt1002.backend;
 
 import java.util.ArrayList;
 
+/**
+ * A class that represents an account.
+ * An account has a name and a balance.
+ *
+ * @author Emil J., Vegard J., Sander S. & Elias T.
+ * @version 0.5 - 19.04.2023
+ */
 public class Account {
     private String accountName;
     private double accountBalance;
+    /**
+     * A list of all the expenses in the account.
+     */
     public static ArrayList<Expense> expenses;
 
+    /**
+     * Constructor for Account.
+     * The account name must not be null or empty.
+     * The account balance must not be negative.
+     *
+     * @param accountName    the name of the account
+     * @param accountBalance the balance of the account
+     */
     public Account(String accountName, double accountBalance){
         if (accountName == null || accountName.isBlank()){
             throw new IllegalArgumentException("Account name cannot be empty");
@@ -19,10 +37,20 @@ public class Account {
         expenses = new ArrayList<Expense>();
     }
 
+    /**
+     * Returns the name of the account.
+     *
+     * @return the name of the account
+     */
     public String getAccountName() {
         return accountName;
     }
 
+    /**
+     * Sets the name of the account.
+     *
+     * @param accountName the name of the account
+     */
     public void setAccountName(String accountName) {
         if (accountName == null || accountName.isBlank()){
             throw new IllegalArgumentException("Account name cannot be empty");
@@ -30,10 +58,20 @@ public class Account {
         this.accountName = accountName;
     }
 
+    /**
+     * Gets the balance of the account.
+     *
+     * @return the balance of the account
+     */
     public double getAccountBalance() {
         return accountBalance;
     }
 
+    /**
+     * Sets the balance of the account.
+     *
+     * @param accountBalance the balance of the account
+     */
     public void setAccountBalance(double accountBalance) {
         if (accountBalance < 0){
             throw new IllegalArgumentException("Account balance cannot be negative");
