b9f5ebb2b5f918650ba6b60f8844c6710afb7b9a
sande <sander@skofsrud.net>
2023-04-19 14:25:41 +0200

    Added javadoc

--- src/main/java/edu/ntnu/idatt1002/GUIStarter.java
+++ src/main/java/edu/ntnu/idatt1002/GUIStarter.java
@@ -1,6 +1,15 @@
 package edu.ntnu.idatt1002;
+
+/**
+ * A class that starts the GUI.
+ */
 public class GUIStarter {
 
+    /**
+     * The main method.
+     *
+     * @param args the command line arguments
+     */
     public static void main(final String[] args){
         Main.main(args);
     }
--- src/main/java/edu/ntnu/idatt1002/Main.java
+++ src/main/java/edu/ntnu/idatt1002/Main.java
@@ -1,21 +1,17 @@
 
 package edu.ntnu.idatt1002;
 
-import edu.ntnu.idatt1002.backend.*;
+import edu.ntnu.idatt1002.backend.Expenses;
+import edu.ntnu.idatt1002.backend.Incomes;
 import edu.ntnu.idatt1002.frontend.GUI;
 import edu.ntnu.idatt1002.frontend.utility.timeofdaychecker;
-import edu.ntnu.idatt1002.model.CSVReader;
-import edu.ntnu.idatt1002.model.ExcelExporter;
 import javafx.application.Application;
 import javafx.stage.Stage;
-import org.apache.commons.math3.analysis.function.Exp;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
 
 
+/**
+ * A class that starts the GUI.
+ */
 public class Main extends Application {
 
 
@@ -24,6 +20,12 @@ public class Main extends Application {
         GUI gui = new GUI();
         gui.start(stage);
     }
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args)  {
 
         Expenses.createTransportation();
