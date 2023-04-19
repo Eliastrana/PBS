3be416189bfb9fe91d389b99c6b02606686632a9
sande <sander@skofsrud.net>
2023-04-19 14:20:47 +0200

    Added noise-effect to button

--- src/main/java/edu/ntnu/idatt1002/frontend/menu/Budget.java
+++ src/main/java/edu/ntnu/idatt1002/frontend/menu/Budget.java
@@ -3,6 +3,7 @@
 package edu.ntnu.idatt1002.frontend.menu;
 
 import edu.ntnu.idatt1002.frontend.GUI;
+import edu.ntnu.idatt1002.frontend.utility.SoundPlayer;
 import edu.ntnu.idatt1002.frontend.utility.timeofdaychecker;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -218,6 +219,10 @@ public class Budget {
           }
         }
       }
+      categoryMenu.setValue("Select Category");
+      budgetAmountField.clear();
+      SoundPlayer.play("src/main/resources/16bitconfirm.wav");
+
     });
 
     String currentMonth = timeofdaychecker.getCurrentMonth();
